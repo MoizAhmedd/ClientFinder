f5df0d2b4ac04f5b82c6fc346f565271dc7f6e2d
MoizAhmedd <43660046+MoizAhmedd@users.noreply.github.com>
2018-12-30 10:38:00 -0500

    Update PlacesClientFinder.py

--- PlacesClientFinder.py
+++ PlacesClientFinder.py
@@ -15,44 +15,45 @@ class potclients:
     def __str__(self):
         print('We found ' +  str(len(self.clients)) + ' location ' + 'without a website')
         return "{}".format(self.clients)
+    
+    def __len__(self):
+        return len(self.clients)
 
 a = potclients()
-wanted_location = input('What Location are you looking for?')
-keywords = input('What keywords are you looking for?')
-max_radius = int(input('How many meters away are you looking for place?'))
+while len(a) < 10:
+    wanted_location = input('What Location are you looking for?')
+    keywords = input('What keywords are you looking for?')
+    max_radius = int(input('How many meters away are you looking for place?'))
 
 
-YOUR_API_KEY = ' '
+    YOUR_API_KEY = 'AIzaSyB1vilYdjlYwchzO-oW31RS7-6fFK8EpDM'
 
-google_places = GooglePlaces(YOUR_API_KEY)
+    google_places = GooglePlaces(YOUR_API_KEY)
 
-    #Finds Places
-query_result = google_places.nearby_search(
-        location=wanted_location, keyword=keywords,
-        radius=max_radius, types=[types.TYPE_FOOD])
-
-
-if query_result.has_attributions:
-    print (query_result.html_attributions)
+        #Finds Places
+    query_result = google_places.nearby_search(
+            location=wanted_location, keyword=keywords,
+            radius=max_radius, types=[types.TYPE_FOOD])
 
 
-for place in query_result.places:
-
-    place.get_details()
-
-    if not place.website: #Checks if place does NOT have a website
-        a.addclient(place.name,place.local_phone_number)
+    if query_result.has_attributions:
+        print (query_result.html_attributions)
 
 
+    for place in query_result.places:
+
+        place.get_details()
+
+        if not place.website: #Checks if place does NOT have a website
+            a.addclient(place.name,place.local_phone_number)
 
 
-    # Are there any additional pages of results?
-if query_result.has_next_page_token:
-    query_result_next_page = google_places.nearby_search(
-            pagetoken=query_result.next_page_token)
 
 
-print(a)
+        # Are there any additional pages of results?
+    if query_result.has_next_page_token:
+        query_result_next_page = google_places.nearby_search(
+                pagetoken=query_result.next_page_token)
 
 
-    # Adding and deleting a place
+    print(a)
